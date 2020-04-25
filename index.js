ac8bbf4bffcfe0b1ad793b51f3596c621695cc52
TranHyKhang <tranhykhang8108@gmail.com>
2020-04-25 22:22:29 +0700

    Create, view, delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var shortid = require('shortid');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -17,17 +18,55 @@ const db = low(adapter)
 db.defaults({ todos: []})
   .write()
 
+//Render on main page
 app.get('/', function(req, res) {
     res.render('index', {
          todos: db.get('todos').value()
     });
 });
 
+
+// Render on create page
 app.get('/create', function(req, res) {
     res.render('create')
 })
 
+// Search item
+app.get('/search', function(req, res) {
+    var q = req.query.q;
+    var matched = db.get('todos').value().filter(item => {
+        return item.text.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    })
+    res.render('index', {
+        todos: matched
+    })
+})
+
+//Delete item
+app.get('/:id/delete', function(req, res) {
+    var id = req.params.id;
+    var delTodo = db.get('todos').find({id: id}).value();
+    console.log(delTodo);
+    db.get('todos').remove({text: delTodo.text}).write();
+    res.render('index', {
+        todos: db.get('todos').value()
+    })
+})
+
+// View item
+app.get('/:id', function(req, res) {
+    var id = req.params.id;
+    var todo = db.get('todos').find({id: id}).value();
+    console.log(todo)
+
+    res.render('view', {
+        todo: todo
+    })
+})
+  
+// Create item
 app.post('/create', function(req, res) {
+    req.body.id = shortid.generate();
     db.get('todos').push(req.body).write();
     res.redirect('/');
 })
